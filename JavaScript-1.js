3ef83bab682de8905a30a22199f95bd0ea67b0a0
keh7114 <60195646+keh7114@users.noreply.github.com>
2020-02-05 10:45:59 -0600

    Add files via upload
    HO 3 Challenge 2

---
+++ JavaScript-1.js
@@ -0,0 +1,7 @@
+     var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
+     floatAge = prompt("Enter your Age");
+     floatDays = floatAge * 365.25;
+     intWeeks = floatDays / 7
+     floatMonths = floatAge*12;
+     floatFortnights = floatDays / 14
+     alert( floatAge, floatDays, intWeeks, floatMonths, floatFortnights);
