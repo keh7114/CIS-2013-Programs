40652107e5616615c3a7ad53e1e1c0a88f0ef9c2
keh7114 <60195646+keh7114@users.noreply.github.com>
2020-02-05 09:55:14 -0600

    Add files via upload
    HO 3 Challenge 1

---
+++ mpg.js
@@ -0,0 +1,5 @@
+     var floatMiles, floatGallons, floatMpg;
+     floatMiles = prompt("Enter miles driven");
+     floatGallons = prompt("Enter gallons of gas used");
+     floatMpg = floatMiles/floatGallons;
+     alert("floatMiles per floatGallon = " + floatMpg);
