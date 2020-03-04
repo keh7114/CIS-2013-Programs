e0fd2065382202eec6aa7f6525ce5b2616a44745
keh7114 <60195646+keh7114@users.noreply.github.com>
2020-03-04 09:14:49 -0600

    Add files via upload

--- JavaScript-1.js
+++ JavaScript-1.js
@@ -1,7 +1,52 @@
-     var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
-     floatAge = prompt("Enter your Age");
-     floatDays = floatAge * 365.25;
-     intWeeks = floatDays / 7
-     floatMonths = floatAge*12;
-     floatFortnights = floatDays / 14
-     alert( floatAge, floatDays, intWeeks, floatMonths, floatFortnights);
+var $ = function (id) 
+{
+    return document.getElementById(id);
+};
+
+var calculate_click = function () 
+{
+    var floatHwPts;
+
+    //Declare variables intGradeOption, floatTotalPts, floatHwPts, floatMidPts, floatFinPts, stringFinalGrade
+//Get user input to determine grade
+//Calculate floatTotalPts (sum of HW and exam pts)
+//Get user input on whether the course is audit pass/fail (1) or letter grade (2)
+//If the grade option is audit, evaluate total score based upon 80% cutoff scorefor pass/fail
+//Else determine letter grade based upon the typical A = 90%, B - 80%, etc.
+//Output stringFinalGrade
+
+var intGradeOption, floatTotalPts, floatHwPts, floatMidPts, floatFinPts, stringFinalGrade;
+
+floatHwPts = parseFloat(prompt("Please enter final HW pts (0-30):"));
+
+floatMidPts = ??
+
+floatFinPts = ??
+
+floatTotalPts = parseFloat(??);
+
+intGradeOption = ??prompt??
+
+if (intGradeOption===1)
+{
+    if(floatTotalPts >= 80)
+
+
+
+
+
+
+
+alert$("final_grade").value=stringFinalGrade;
+    
+    floatHwPts = parseFloat($("hw_pts").value);
+
+
+};
+
+window.onload = function () 
+{
+    $("final_grade").value = ""; //blanks the final grade text box upon page load
+    $("calculate").onclick = calculate_click; //activates main method when the button is clicked 
+    $("hw_pts").focus(); //puts the cursor on the first DOM text input box
+};
